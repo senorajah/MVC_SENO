754afec91309a75bca2a28d5c239fd6f877aa7d2
senorajah <85495612+senorajah@users.noreply.github.com>
2023-03-24 04:01:00 -0700

    Update Main.java

--- person/Main.java
+++ person/Main.java
@@ -1,20 +1,17 @@
 package person;
 public class Main {
     public static void main(String[] args) {
-        // Create a new person
-        Person person = new Person("John", 30);
         
-        // Create a view for that person
+        Person person = new Person("John", 30);
+       
         PersonView personView = new PersonView();
         
-        // Create a controller to handle user input and update the view
+       
         PersonController personController = new PersonController(person, personView);
         
-        // Update the person's name and age
         personController.setPersonName("Jane");
         personController.setPersonAge(25);
-        
-        // Update the view with the new person details
+       
         personController.updateView();
     }
 }
